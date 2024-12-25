b9506354c7a8b1e52db40372888b9b28bfdcc38d
kaetaen <kaetaen@tutanota.com>
2024-12-25 15:19:38 -0300

    ✅ tests: add id and createOn tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,21 +2,40 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	name     = "Campaign X"
+	content  = "This is a test campaign."
+	contacts = []string{"julia_contact@google.com", "paul_contact@mail.com"}
 )
 
 func TestNewCampaign(t *testing.T) {
 	assert := assert.New(t)
 
-	name := "Campaign X"
-	content := "This is a test campaign."
-	contacts := []string{"julia_contact@google.com", "paul_contact"}
-
 	campaign := NewCampaign(name, content, contacts)
 
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IdIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
+	assert := assert.New(t)
+
+	now := time.Now().Add(-time.Minute)
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreateOn, now)
 }
